808bd751a1606b19f644d688becb410f53d963ab
mika <mikasa2511592510@gmail.com>
2026-04-12 19:08:01 +0800

    公式支持，更新配置文件和依赖项，新增博客文章《博客和笔记的区别》和《毕设总结》，优化样式。import KaTeX support, update config and dependencies, add new blog posts "Difference between Blog and Notes" and "Thesis Summary", optimize styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://erinmi4.github.io",
   integrations: [sitemap()],
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: "github-dark-default"
     }
